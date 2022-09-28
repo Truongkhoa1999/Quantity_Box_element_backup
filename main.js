9a7d6ae57777b7fb18f4931e968c4fc615931040
Truongkhoa1999 <113099964+Truongkhoa1999@users.noreply.github.com>
2022-09-28 21:48:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var a = 0;
+document.getElementById("root").innerText = a;
+function deFunction() {
+    a -= 1;
+    document.getElementById("root").innerText = a;
+}
+function inFunction() {
+    a += 1;
+    document.getElementById("root").innerText = a;
+}
